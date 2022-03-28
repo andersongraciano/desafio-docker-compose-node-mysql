0817942a22ab73726b48f0e2dd55d4347540ef7d
Anderson Graciano <andersongra@yahoo.com.br>
2022-03-28 15:57:21 -0300

    criar tabela automaticamente

--- node/peoplecreate.js
+++ node/peoplecreate.js
@@ -9,7 +9,7 @@ const config = {
 
 const createTable = () => {
     var con = mysql.createConnection(config)
-    connection.query('CREATE TABLE IF NOT EXISTS people (id int not null auto_increment, name VARCHAR(255), primary key(id))')
+    con.query('CREATE TABLE IF NOT EXISTS people (id int not null auto_increment, name VARCHAR(255), primary key(id))')
     con.end()
 }
 
